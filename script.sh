c662b46e6cb2fa05dee5bad0620badd41eed6c23
Pasi Uotinen <actionpaz@yahoo.com>
2024-08-11 15:57:21 +0300

    Initial commit

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+while true; do
+    name=$(dialog --keep-tite --title 'Important information' --inputbox 'Please enter the name: ' 10 40 2>&1 >/dev/tty)
+    if (( $? != 0 )); then
+        break
+    elif [[ -n "${name}" ]]; then
+        echo "${name}" >> names.txt
+    fi
+    if ! dialog --keep-tite --yesno 'Do you wish to continue?' 0 0; then
+        break
+    fi
+done
